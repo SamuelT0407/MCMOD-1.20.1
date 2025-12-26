0b0f01a80fc2d9b65e20b59d5c32cff386c22cb3
SamuelT0407 <samueltongjeehang@gmail.com>
2025-12-26 09:11:23 +0800

    fix custom gas

--- kubejs/startup_scripts/test_all_extensions.js
+++ kubejs/startup_scripts/test_all_extensions.js
@@ -16,15 +16,3 @@ StartupEvents.registry('mekanism:infuse_type', event => {
 StartupEvents.registry('mekanism:pigment', event => {
     event.create('custom_pigment').color(0x00BFFF).displayName('自定义色素') // 深天蓝
 })
-
-StartupEvents.registry('mekanism:slurry', event => {
-    event.create('custom_slurry_dirty', 'dirty').color(0x8B4513).displayName('自定义浑浊浆液') // 浑浊的棕色
-})
-
-StartupEvents.registry('mekanism:slurry', event => {
-    event.create('custom_slurry_clean', 'clean').color(0xFFD700).displayName('自定义纯净浆液') // 纯净的金色
-})
-
-StartupEvents.registry('mekanism:slurry', event => {
-    event.create('custom_slurry_basic', 'basic').color(0xC0C0C0).displayName('自定义基础浆液') // 基础银灰色
-})
